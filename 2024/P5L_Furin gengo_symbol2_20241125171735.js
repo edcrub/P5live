2130dec98710142f6891b6b7497e97363cd46f70
6.n.h8 <ed_crub@live.com>
2024-11-25 17:33:54 +0700

    Add files via upload

---
+++ 2024/P5L_Furin gengo_symbol2_20241125171735.js
@@ -0,0 +1,47 @@
+// {"P5LIVE":{"name":"Furin gengo_symbol2","mod":1732555055921}} 
+
+let elementsX = 11
+let elementsY = 15
+let font = googleFont('Noto Sans Symbols 2')
+var charset = '▛▚▚░▒▓'
+
+function googleFont(fontName) {
+	let link = document.createElement("link")
+	link.href = "https://fonts.googleapis.com/css?family=" + encodeURI(fontName) + '&display=swap'
+	link.rel = "stylesheet"
+	document.head.appendChild(link)
+	return fontName
+}
+
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	textFont(font)
+	textAlign(CENTER, CENTER)
+}
+
+function draw() {
+	background(0)
+		fill(255)
+	// noStroke()
+		frameRate(20)
+	var elements = 10
+	
+	for (let x = 0; x < elementsX; x++){
+		for (let y = 0; y < elementsY +1 ; y++){
+			let posY = map(y, 0, elementsY, 0, height)
+			let magX = map(sin(radians(posY*50 + frameCount)),-1,1,-width/2,width/2) // try posY * 50 ,30 ,60
+			let posX = map(x, 0, elementsX, -magX, magX) 
+		
+	
+	let selector = int(random(5)) // try number 1 -5
+
+	push()
+	translate(windowWidth/2 + posX,posY)
+	textSize(45)
+	text(charset[selector],0,0)
+	pop()
+	}
+	}
+}
